575c4392901ef496a0419dc3d0d0bb08fb7d8986
Evgenii Zhitnikov <124378718+ek-zhitnikov@users.noreply.github.com>
2023-05-15 11:05:24 +0400

    Initial Commit

---
+++ Time for a trip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Time for a trip
+//
+//  Created by Евгений Житников on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
